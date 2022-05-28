2ae9a8fa2b788e172e1f545bb7cfa70c8c6f3df5
Rodrigo Linfati <rodrigo@linfati.cl>
2022-05-27 23:41:56 -0400

    x

---
+++ notebooks/999-xyz/xyz.jl
@@ -0,0 +1,40 @@
+### A Pluto.jl notebook ###
+# v0.19.4
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 36184f24-de30-11ec-3a6f-6962a488690c
+begin
+    import Pkg
+    Pkg.UPDATED_REGISTRY_THIS_SESSION[] = true
+    Pkg.activate(; temp = false)
+    Pkg.add(
+        [
+            Pkg.PackageSpec("Plots")
+            Pkg.PackageSpec("JuMP")
+            Pkg.PackageSpec("GLPK")
+            Pkg.PackageSpec("HiGHS")
+            Pkg.PackageSpec("SCIP")
+            Pkg.PackageSpec("DataFrames")
+            Pkg.PackageSpec("Distributions")
+            Pkg.PackageSpec("CSV")
+            Pkg.PackageSpec("JSON")
+            Pkg.PackageSpec("XLSX")
+            Pkg.PackageSpec("Graphs")
+            Pkg.PackageSpec("GraphPlot")
+            Pkg.PackageSpec("LightOSM")
+        ],
+    )
+    Pkg.status()
+    md"""
+    **NOTE:** remove this cell/code
+    **NOTA:** remover esta celda/codigo
+    """
+end
+
+# ╔═╡ 36184f56-de30-11ec-27e2-3f3508fd6cfb
+
+# ╔═╡ Cell order:
+# ╠═36184f24-de30-11ec-3a6f-6962a488690c
+# ╠═36184f56-de30-11ec-27e2-3f3508fd6cfb
